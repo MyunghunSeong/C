820adaee1e974193c6d2503e201eb1ed37f6ee48
성명훈 <gns53869107@gmail.com>
2018-02-19 14:42:10 +0900

    클래스의 상속과 오버라이딩 객체형 변환 함수(is와 as)의 사용예시
    Signed-off-by: 성명훈 <gns53869107@gmail.com>

---
+++ 클래스/상속과클래스의형식변환(is와as)/상속과클래스의형식변환(is와as)/Parents.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 상속과클래스
+{
+    class Pirate //부모 클래스(해적)
+    {
+        protected string name; //맴버 변수
+        protected string fruit;
+
+        public Pirate(string name) //생성자(부모)
+        {
+            Console.WriteLine(name + "이(가) 해적이 되었습니다.");
+            this.name = name;
+        }
+
+        public virtual void EatFruit(string fruit) //virtual -> 상속받는 클래스(자식)에서 재정의(override)하도록 명시해놓은 것
+        {
+            Console.WriteLine(fruit + "열매를 발견하지 못했습니다.");
+            this.fruit = fruit;
+        }
+
+        public virtual void LearnAmbition(params string[] ambitions)
+        {
+            foreach(string ability in ambitions)
+            {
+                Console.Write(ability);
+                Console.WriteLine(" 패기를 배울 수 없습니다.");
+            }
+        }
+
+        public virtual void UseAmbition()
+        {
+                Console.WriteLine(" 패기를 사용할 수 없습니다.");
+        }
+
+        public virtual void Attack()
+        {
+            Console.WriteLine(name + "이(가) 공격합니다!!");
+        }
+
+        public void RideShip()
+        {
+            Console.WriteLine("배틀 탑니다.(해적용)");
+        }
+    }
+
+    class Competent : Pirate //자식 클래스(능력자)
+    {
+        private List<string> ambition_list;
+
+        public Competent(string name) : base(name) //부모 생성자를 상속받는다.(base는 부모 클래스를 의미)
+        {            
+            Console.WriteLine(name + "이(가) 능력자가 되었습니다.");
+            this.name = name;
+            fruit = string.Empty;
+            ambition_list = new List<string>();
+        }
+
+        public new void RideShip() //부모 클래스의 RideShip메소드를 숨김(자식 클래스만의 메소드를 정의)
+        {
+            Console.WriteLine("배를 탑니다.(능력자)");
+            Console.WriteLine();
+        }
+
+        public override void EatFruit(string fruit)  // 부모 클래스의 EatFruit메소드 재정의(오버라이딩) 
+                                                     // 오버라이딩의 조건 1. 부모클래스 : virtual이나 abstract 키워드로 선언되어야 한다.
+                                                     //                   2. 자식클래스 : override의 키워드를 사용해서 부모클래스의 메소드를 재정의(오버라이딩)한다.  
+        {
+            Console.WriteLine(fruit + " 열매섭취!!");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(name + "이(가) " + fruit + " 열매 능력자가 되었습니다!!");
+            Console.WriteLine();
+
+            this.fruit = fruit;
+        }
+
+        public override void LearnAmbition(params string[] ambitions)
+        {
+            for (int i = 0; i < ambitions.Length; i++)
+            {
+                ambition_list.Add(ambitions[i]);
+            }
+        }
+
+        public override void UseAmbition()
+        {
+            foreach(string ability in ambition_list)
+            {
+                Console.Write(ability + " ");
+                Console.WriteLine("패기를 사용했습니다.");
+            }
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine(name + fruit + "공격개시!!");
+            Console.WriteLine();
+        }
+
+        public string SetName { set { this.name = value; } }
+    }
+}
---
+++ 클래스/상속과클래스의형식변환(is와as)/상속과클래스의형식변환(is와as)/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/***********************************************
+ * 
+ *      상속과오버라이딩그리고is와as
+ *        
+ *       1. 상속의 예시(Parents클래스 참조)
+ *       2. 오버라이딩의 구현(Parents클래스 참조)
+ *       2. is와  as
+ * 
+ * *********************************************/
+namespace 상속과클래스
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pirate luffy = new Competent("루피");
+            luffy.EatFruit("고무고무");
+            luffy.RideShip(); //부모 클래스 형식의 luffy객체는 부모클래스의 RideShip()이 출력
+            luffy.LearnAmbition("패왕", "견문", "무장");
+            luffy.UseAmbition();
+            luffy.Attack();
+
+            Console.WriteLine();
+
+            Competent ace = new Competent("에이스"); //Competent클래스 형식으로 생성
+            ace.EatFruit("이글이글");
+            ace.RideShip(); //Competent 형식의 에이스는 new 키워드를 사용해 새로 정의한 RideShip()이 호출
+            ace.LearnAmbition("패왕", "견문", "무장");
+            ace.UseAmbition();
+            ace.Attack();
+
+            Competent aokizi;
+
+            if (ace is Competent) //ace객체가 Competent형식이면 true, 아니면 false를 반환
+                                  //형변환 실패 : 예외를 발생시킴
+            {
+                aokizi = (Competent)luffy;
+                aokizi.SetName = "아오키지";
+                aokizi.RideShip();
+                aokizi.EatFruit("얼음얼음");
+                aokizi.UseAmbition();
+                aokizi.Attack();
+            }
+
+            Competent akainu = ace as Competent; //ace객체를 Competent객체 형식으로 변환해서 Competent형식의 akainu객체에 대입
+                                                 //성공하면 객체를 반환, 아니면 null값을 반환
+                                                 //형변환 실패 : 객체 참조를 null로 만듬
+
+            if (akainu != null)
+            {
+                akainu.SetName = "아카이누";
+                akainu.EatFruit("마그마그");
+                akainu.RideShip();
+                akainu.LearnAmbition("견문");
+                akainu.UseAmbition();
+                akainu.Attack();
+            }
+            
+        }
+    }
+}

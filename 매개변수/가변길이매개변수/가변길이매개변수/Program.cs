cbdc5680b55ce516258da6b2343a06e9e280e3e9
성명훈 <gns53869107@gmail.com>
2018-02-19 10:40:13 +0900

    매개변수가 가변적으로 변할 때 이를 해결하기 위한 방법과 가변길이 매개변수의 사용예시
    Signed-off-by: 성명훈 <gns53869107@gmail.com>

---
+++ 매개변수/가변길이매개변수/가변길이매개변수/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/***********************************************
+ * 
+ *          가변길이 매개변수
+ *      1. params 키워드 사용
+ * 
+ * *********************************************/
+namespace 가변길이매개변수
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== 매개변수 3개 ===");
+            new Program().sum(1, 2, 3);
+            Console.WriteLine("=== 매개변수 4개 ===");
+            new Program().sum(1, 2, 3, 4);
+            Console.WriteLine("=== 매개변수 5개 ===");
+            new Program().sum(1, 2, 3, 4, 5);
+        }
+
+        /* 1. params 키워드 사용 */
+        private void sum(params int[] arr) //가변적으로 들어오는 매개변수에 대해서 params키워드를 사용하여 해결 
+        {
+            int sum = 0;
+            foreach(int value in arr)
+            {
+                sum += value;
+            }
+
+            Console.WriteLine("size of arr = " + arr.Length);
+            Console.WriteLine("result = " + sum);
+            Console.WriteLine();
+        }
+    }
+}

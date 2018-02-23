17dc8584ed066769fef33853172170f8e09d16c7
성명훈 <gns53869107@gmail.com>
2018-02-23 11:35:32 +0900

    예외처리(try~catch문)을 사용한 예제
    Signed-off-by: 성명훈 <gns53869107@gmail.com>

---
+++ 예외처리/예외처리(try~catch)/DivideException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/***********************************************
+ * 
+ *          예외처리(try~catch)
+ *          
+ *        1. try~catch문 사용
+ *        2. 예외 발생 상황
+ * 
+ * *********************************************/
+namespace 예외처리_try_catch_
+{
+    class DivideException
+    {
+        public void Div(ref int num1, ref int num2, ref double result)
+        {
+            try // --- 1. try문 안에 해당 작업을 작성한다.
+            {
+                result = num1 / num2; 
+            }
+            catch (DivideByZeroException e) // --- 1. 나누는 수(num2)가 0일 때 발생하는 예외에 대해서 catch문에서 처리한다.
+            {
+                Console.WriteLine("0으로 나눌 수 없습니다."); //메시지 출력
+            }
+        }
+    }
+}
---
+++ 예외처리/예외처리(try~catch)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 예외처리_try_catch_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 0; 
+            double result = 0;
+
+            DivideException div = new DivideException();
+
+            div.Div(ref num1, ref num2, ref result); //num2가 0이면 예외발생
+
+            Console.WriteLine("result = " + result);
+        }
+    }
+}

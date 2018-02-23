97220ca80f7cab3fe52623341c85931fb012ff69
성명훈 <gns53869107@gmail.com>
2018-02-23 14:10:02 +0900

    예외던지기(throw문)을 이용한 예외처리 방법 예제
    Signed-off-by: 성명훈 <gns53869107@gmail.com>

---
+++ 예외처리/예외던지기(throw문)/ExceptionThrow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 예외던지기_throw문_
+{
+    class ExceptionThrow
+    {
+        public void Div(int a, int b)
+        {
+            int result = 0;
+
+            if (b != 0)
+                result = a / b;
+            else
+                throw new DivideByZeroException(); //Div함수에게 예외가 던저진다. => Div함수를 호출한 쪽에서 예외를 처리
+        }
+    }
+}
---
+++ 예외처리/예외던지기(throw문)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/***********************************************
+ * 
+ *          출력전용매개변수
+ *          
+ *        1. throw된 예외 처리하기
+ * 
+ * *********************************************/
+namespace 예외던지기_throw문_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = string.Empty;
+            string num2 = string.Empty;
+
+            ExceptionThrow throw_ex = new ExceptionThrow();
+
+            Console.Write("정수1을 입력하세요 : ");
+            num1 = Console.ReadLine();
+            Console.Write("정수2를 입력하세요 : ");
+            num2 = Console.ReadLine();
+
+            try
+            {
+                throw_ex.Div(Convert.ToInt32(num1), Convert.ToInt32(num2)); // --- 1. Div함수를 호출(기능실행)
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message); // --- 1. Div함수에서 throw한 예외를 받아 처리한다.
+            }
+        }
+    }
+}

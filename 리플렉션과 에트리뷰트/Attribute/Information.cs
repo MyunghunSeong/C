932e1f7e3fc1b3fb374268e05bae943a80cd1388
성명훈 <gns53869107@gmail.com>
2018-02-27 02:04:48 +0900

    애트리뷰트의 선언과 활용예제

---
+++ 리플렉션과 에트리뷰트/Attribute/Information.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Attribute_Example.cs
+{
+    [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
+    class Information :System.Attribute
+    {
+        private string program;
+        private string author;
+        private double version;
+        private DateTime date;
+
+        public Information(string program)
+        {
+            this.program = program;
+            author = string.Empty;
+            version = 0;
+            date = DateTime.Now;
+        }
+
+        public string Program 
+        {
+            get { return program;}
+            set { program = value;} 
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public double Version
+        {
+            get { return version; }
+            set { version = value; }
+        }          
+    }
+}
---
+++ 리플렉션과 에트리뷰트/Attribute/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/***********************************************
+* 
+*               에트리뷰트
+*          
+*           1. 에트리뷰트 사용
+*           2. 주석과 차이점
+* 
+* *********************************************/
+namespace Attribute_Example.cs
+{
+    [Information("Attribut_Example_Program", Author="성명훈", Version=1.0)] // --- 1. 부가적인 정보를 나타낼 때 사용
+    [Information("Attribute_Example_program", Author="성현아", Version=1.1)] // --- 2. 주석 : 사람이 쓰고 사람이 읽음                                                                         //        에트리뷰트 : 사람이 쓰고 컴퓨터가 읽음
+    class Program
+    {
+        static void Main(string[] args) //Main Func()
+        {
+            Type type = typeof(Program);
+            Attribute[] attributes = Attribute.GetCustomAttributes(type);
+
+            Console.WriteLine("=== Main Information ===");
+
+            foreach (Attribute att in attributes)
+            {
+                Information info = att as Information;
+                if (info != null)
+                    Console.WriteLine("Program : " + info.Program + "\n" +
+                                      "Author : " + info.Author + "\n" +
+                                      "Version : " + info.Version + "\n" +
+                                      "Date : " + DateTime.Now);
+                Console.WriteLine();
+            }
+
+           
+        }
+    }
+}
